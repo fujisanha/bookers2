928a20bc6c124dd0a96480dafef11b3c28cc0937
EC2 Default User <ec2-user@ip-172-31-5-74.ap-northeast-1.compute.internal>
2024-06-25 05:05:56 +0000

    [add] RSpec<%=render...user:@user%> コントローラーに @user=@book.user でエラー解決

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,8 +22,8 @@ class BooksController < ApplicationController
   end
 
   def show
-    # @user = current_user.id
     @book = Book.find(params[:id])
+    @user = @book.user
   end
   
   def destroy
